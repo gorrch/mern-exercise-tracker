8df200b33b9ce61d421e922b6b3c7c29a10ba609
igor chlewicki <igor.chlewicki@opennet.pl>
2020-09-21 13:22:29 +0200

    first mern commit

---
+++ src/components/edit-exercise.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class EditExercise extends Component {
+    render(){
+        return (
+            <div>
+                <p>You are one the Edit Exercise List component!</p>
+            </div>
+        )
+    }
+}
